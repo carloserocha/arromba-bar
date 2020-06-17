1ff2e25df4704226cc06e21ebe500b1693ef5304
carloserocha <carloserocha27@gmail.com>
2020-06-17 18:53:58 -0300

    mudanças no carrinho

--- src/webapp.py
+++ src/webapp.py
@@ -10,18 +10,30 @@ app = Flask(__name__,
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/bar'
 
 client_db = PyMongo(app)
-
 client_db = client_db.db
-
 
 @app.route('/')
 def indexPage():
     products = [ product for product in client_db.Product.find({ 'status': True }) ]
-    return render_template('index.html.jinja', products=products)
+    return render_template('index.html', products=products)
 
-@app.route('/checkout', methods=['POST'])
+@app.route('/admin', methods=['GET'])
 def checkoutPage():
-    return render_template('checkout.html')
+    # sort -> o último criado até o ultimo
+    orders = [order for order in client_db.Order.find().sort('order_id', -1)]
+    members = []
+    for order in orders:
+        member = client_db.Member.find_one({'client_id': order['client_id']})
+        for product in order['products']:
+            find = client_db.Product.find_one({ 'sku': int (product['sku']) })
+            product['name'] = find['name']
+        members.append(member)
+
+    return render_template('admin.html', members=members, orders=orders)
+
+@app.route('/customer', methods=['GET'])
+def customerPage():
+    return render_template('customer.html')
 
 @app.route('/process/order', methods=['POST'])
 def processOrder():
@@ -36,7 +48,7 @@ def processOrder():
 
     order_id = [
             doc['order_id'] for doc in 
-                client_db.Order.find().limit(1).sort('created_at', -1)
+                client_db.Order.find().limit(1).sort('_id', -1)
         ] or [10001]
     
     order_id = order_id[0] + 1
